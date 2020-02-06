e2c8612fb918da82f184d18aa38c9ac0371c3aab
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-06 16:26:58 -0700

    added doc blocks to User class

---
+++ php/Classes/User.php
@@ -0,0 +1,22 @@
+<?php
+
+	namespace PawsForCause\Paws;
+
+	require_once ("autoload.php");
+	require_once (dirname(__DIR__)) . "/vendor/autoload.php";
+
+	use Ramsey\Uuid\Uuid;
+
+	/**
+    * User Class
+    *
+    * This is the user class for the Capstone Project - Paws for the Cause.
+    *
+    * @author Matthew Urrea <matt.urrea.code@gmail.com>
+    *
+    **/
+
+class User{
+
+   
+}
