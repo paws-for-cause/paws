d015943c05a48fd7b3b9e772cedec36dec94f1c5
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-07 00:30:43 -0700

    added accessor and mutator methods

--- php/Classes/User.php
+++ php/Classes/User.php
@@ -72,11 +72,11 @@ class User{
     * constructor method for user
     * @param $userId id for the user
     * @param $userActiviationToken activation token for the author
-    * @param $userAge integer for user age
+    * @param $userAge age of user
     * @param $userDescription description of user
     * @param $userEmail user email
     * @param $userFirstName user first name
-    * @param $userGender gender of the user
+    * @param $userGender user gender
     * @param $userHash password for the user
     * @param $userLastName user last name
     * @param $userPhone user phone number
@@ -124,14 +124,205 @@ class User{
    }
 
    /**
-    * accessor method for a
+    * accessor method for user activation token
     *
-    ** /
+    * @return Uuid value of activation token
+    **/
+   public function getUserActivationToken(): ?string {
+      return ($this->getUserActivationToken);
+   }
 
+   /**
+    * mutator method for author activation token
+    *
+    * @param string $newUserActivationToken
+    * @throws \InvalidArgumentException if the token is not a string or insecure
+    * @throws \RangeException if $newUserActivationToken is not exactly 32 characters
+    * @throws \TypeError if $newUserActivationToken is not a string
+    **/
+   public function setUserActivationToken(?string $newUserActivationToken) : void {
+      if($newUserActivationToken === null){
+         $this->userActivationToken = null;
+         return;
+      }
+      $newUserActivationToken = strolower(trim($newUserActivationToken));
+      if(ctype_xdigit($newUserActivationToken) === false) {
+         throw(new\RangeException("user activation token is not valid"));
+      }
+      $this->userActivationToken = $newUserActivationToken;
+   }
 
+   /**
+    * accessor method for user age
+    *
+    * @return integer value of the user description
+    **/
+   public function getUserAge() : int {
+      return ($this->userAge);
+   }
 
+   /**
+    * mutator method for user age
+    *
+    * @param integer $newUserAge new value of the users age
+    * @throws \InvalidArgumentException if $newUserAge is not a integer or insecure
+    * @throws \RangeException if $newUserAge is < 120
+    * @throws \TypeError if $newUserAge is not a integer
+    **/
+   public function setUserAge(int $newUserAge) : void {
+      //verify the age integer is secure
+      $newUserAge = trim($newUserAge);
+      $newUserAge = filter_var($newUserAge, FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_NO_ENCODE_QUOTES);
+      if(empty($newUserAge)===true){
+         throw(new \InvalidArgumentException("Age value empty or insecure"));
+      }
+      //verify age
+      if(is_integer($newUserAge) < 120){
+         throw (new \RangeException("Age value too large"))
+      }
+      //store the age value
+      $this->userAge= $newUserAge;
+   }
 
+   /**
+    * accessor method for user description
+    *
+    * @return string value of the user description
+    **/
+   public function getUserDescription() : string {
+      return ($this->userDescription);
+   }
 
+   /**
+    * mutator method for user description
+    *
+    * @param string $newUserDescription new value of description content
+    * @throws \InvalidArgumentException if $newUserDescription is not a string or insecure
+    * @throws \RangeException if $newUserDescription is > 200 characters
+    * @throws \TypeError if $newUserDescription is not a string
+    **/
+   public function setUserDescription(string $newUserdescription) : void {
+      //verify the description content is secure
+      $newUserdescription = trim($newUserdescription);
+      $newUserdescription = filter_var($newUserdescription, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      if(empty($newUserdescription)===true){
+         throw(new\InvalidArgumentException("Description is empty or insecure"));
+      }
+      //verify user description will fit in the database
+      if(strlen($newUserdescription) > 200) {
+         throw(new \RangeException("Description is too long"));
+      }
+      //store user description
+      $this->userDescription = $newUserdescription;
 
+   }
 
+   /**
+    * accessor method for user email
+    *
+    * @return string value of user email
+    **/
+    public function getUserEmail() : string {
+       return($this->userEmail);
+    }
+
+   /**
+    * mutator method for user email
+    *
+    * @param string $newUserEmail
+    * @throws \InvalidArgumentException if the email is not a string or insecure
+    * @throws \RangeException if $newUserEmail is not over 64 characters
+    * @throws \TypeError if $newUserEmail is not a string
+    **/
+   public function setUserEmail(string $newUserEmail) : void {
+      //verify that the user email is secure
+      $newUserEmail = trim($newUserEmail);
+      $newUserEmail = filter_var($newUserEmail, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      if(empty($newUserEmail)===true) {
+         throw(new \InvalidArgumentException("User email is empty or insecure"));
+      }
+         //verify that the user email will fit in the database
+         if(strlen($newUserEmail) > 64) {
+            throw(new \RangeException("User email is too long"));
+         }
+
+         //store the user email
+         $this->userEmail = $newUserEmail;
+      }
+
+   /**
+    * accessor method for user first name
+    *
+    * @return string value of user first name
+    **/
+    public function getUserFirstName() : string {
+       return ($this->userFirstName);
+    }
+
+   /**
+    * mutator method for user first name
+    *
+    **/
+   public function setUserFirstName(string $newUserFirstName) : void {
+      //verify that the user email is secure
+      $newUserFirstName = trim($newUserFirstName);
+      $newUserFirstName = filter_var($newUserFirstName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+      if(empty($newUserFirstName)===true) {
+         throw(new \InvalidArgumentException("User first name is empty or insecure"));
+      }
+      //verify that the user email will fit in the database
+      if(strlen($newUserFirstName) > 32) {
+         throw(new \RangeException("User first name is too long"));
+      }
+
+      //store the user email
+      $this->userFirstName = $newUserFirstName;
+   }
+
+   /**
+    * accessor method for user gender
+    *
+    * @return string value of user gender
+    **/
+    public function getUserGender() : string {
+       return($this->userGender);
+    }
+   /**
+    * mutator method for user gender
+    *
+    **/
+
+   /**
+    * accessor method for user password
+    *
+    **/
+    public function getUserHash() : string {
+       return ($this->userHash);
+    }
+   /**
+    * mutator method for user password
+    *
+    **/
+
+   /**
+    * accessor method for user last name
+    *
+    **/
+    public function getUserLastName() : string {
+    }
+               }
+   /**
+    * mutator method for user last name
+    *
+    **/
+
+   /**
+    * accessor method for user phone number
+    *
+    **/
+
+   /**
+    * mutator method for user phone number
+    *
+    **/
 }
