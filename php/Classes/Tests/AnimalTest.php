12c76d4d66b6f31607c5a75c3ba3c5e219f51495
Thomas Dameron <tdameron1@cnm.edu>
2020-02-11 16:03:05 -0700

    Created initial parameters for Unit Test.

---
+++ php/Classes/Tests/AnimalTest.php
@@ -0,0 +1,74 @@
+<?php
+namespace PawsForCause\Paws\Tests;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
+
+// grab the encrypted properties file
+require_once("/etc/apache2/ddl.sql/Secret.php");
+
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+/**
+ * Full PHPUnit test for the Animal class
+ *
+ * This is the Unit Test for the animal class for the Capstone Project
+ *
+ * @see Animal
+ * @author Thomas Dameron <tdameron1@cnm.edu>
+ */
+class AnimalTest extends PawsTest {
+	/**
+	 * animalId that is in the database; this is for foreign key relations
+	 * @var Animal animalId
+	 */
+	protected $animal = null;
+
+
+	/**
+	 * valid animal Shelter Id that is related to the animal
+	 * @var $VALID_ANIMAL_SHELTER_ID;
+	 **/
+	protected $VALID_ANIMAL_SHELTER_ID;
+
+	/**
+	 * Animal Adoption Status, whether or not the animal has been adopted
+	 * @var string $VALID_ANIMAL_ADOPTION_STATUS
+	 **/
+	protected $VALID_ANIMAL_ADOPTION_STATUS ="PHPUnit test passing";
+
+	/**
+	 * The breed of the animal
+	 * @var string $ANIMAL_BREED
+	 **/
+	protected $VALID_ANIMAL_BREED = "PHPUnit test passing";
+
+	/**
+	 * The gender of the animal
+	 * @var string $VALID_ANIMAL_GENDER
+	 **/
+	protected $VALID_ANIMAL_GENDER = "PHPUnit test passing";
+
+	/**
+	 * The name of the animal
+	 * @var string $VALID_ANIMAL_NAME
+	 **/
+	protected $VALID_ANIMAL_NAME ="PHPUnit test passing";
+
+	/**
+	 * The Photo URL of the anima
+	 * @var string $VALID_ANIMAL_PHOTO_URL;
+	 **/
+	protected $VALID_ANIMAL_PHOTO_URL = "PHPUnit test passing";
+
+	/**
+	 * The animal species
+	 * @var string $VALID_ANIMAL_SPECIES;
+	 **/
+	protected $VALID_ANIMAL_SPECIES = "PHPUnit test passing";
+
+
+}
