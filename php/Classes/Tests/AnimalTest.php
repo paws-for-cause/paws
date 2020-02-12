0271c7a040113d22897da4846080a7e38b7ef44c
Thomas Dameron <tdameron1@cnm.edu>
2020-02-12 11:48:46 -0700

    completed testGetValidAnimalByLikeUserId.

--- php/Classes/Tests/AnimalTest.php
+++ php/Classes/Tests/AnimalTest.php
@@ -117,7 +117,7 @@ class AnimalTest extends PawsTest {
 	/**
 	 * test inserting an animal, editing it, and then updating it
 	 */
-	public function testUpdateValidAnimal() : void {
+	public function testUpdateValidAnimal(): void {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("animal");
 
@@ -138,7 +138,7 @@ class AnimalTest extends PawsTest {
 	/**
 	 * A test for putting a new animal and then deleting it
 	 */
-	public function testDeleteValidAnimal() : void {
+	public function testDeleteValidAnimal(): void {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("animal");
 
@@ -186,35 +186,65 @@ class AnimalTest extends PawsTest {
 		$this->assertEquals($pdoAnimal->getAnimalSpecies(), $this->VALID_ANIMAL_SPECIES);
 	}
 
-		/**
-		 * Test grabbing animal by Shelter Id
-		 **/
-		public function testGetValidAnimalByShelterId() : void {
-			// count the number of rows and save it for later
-			$numRows = $this->getConnection()->getRowCount("animal");
+	/**
+	 * Test grabbing animal by Shelter Id
+	 **/
+	public function testGetValidAnimalByShelterId(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("animal");
 
-			// create a new animal and insert to mySQL
-			$animalId = generateUuidV4();
-			$animal = new Animal($animalId, $this->shelter->getShelterId(), $this->animal->getAnimalId(), $this->VALID_ANIMAL_SHELTER_ID, $this->VALID_ANIMAL_ADOPTION_STATUS, $this->VALID_ANIMAL_BREED, $this->VALID_ANIMAL_GENDER, $this->VALID_ANIMAL_NAME, $this->VALID_ANIMAL_PHOTO_URL, $this->VALID_ANIMAL_SPECIES);
-			$animal->insert($this->getPDO());
+		// create a new animal and insert to mySQL
+		$animalId = generateUuidV4();
+		$animal = new Animal($animalId, $this->shelter->getShelterId(), $this->animal->getAnimalId(), $this->VALID_ANIMAL_SHELTER_ID, $this->VALID_ANIMAL_ADOPTION_STATUS, $this->VALID_ANIMAL_BREED, $this->VALID_ANIMAL_GENDER, $this->VALID_ANIMAL_NAME, $this->VALID_ANIMAL_PHOTO_URL, $this->VALID_ANIMAL_SPECIES);
+		$animal->insert($this->getPDO());
 
-			// grab the data from mySQL and enforce the fields match our expectations
-			$results = Animal::getAnimalByShelterId ($this->getPDO(), $animal->getAnimalShelterId());
-			$this->assertEquals($numRows +1, $this->getConnection()->getRowCount("animal"));
-			$this->assertCount(1, $results);
-			$this->assertContainsOnlyInstancesOf("PawsForCause\Paws\Animal", $results);
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Animal::getAnimalByShelterId($this->getPDO(), $animal->getAnimalShelterId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("animal"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("PawsForCause\Paws\Animal", $results);
 
-			// grab the result from the array and validate it
-			$pdoAnimal = $results[0];
+		// grab the result from the array and validate it
+		$pdoAnimal = $results[0];
 
-			$this->assertEquals($pdoAnimal->getAnimalId(), $animalId);
-			$this->assertEquals($pdoAnimal->getAnimalId(), $this->shelter->getAnimalShelterId());
-			$this->assertEquals($pdoAnimal->getAnimalAdoptionStatus(), $this->VALID_ANIMAL_ADOPTION_STATUS);
-			$this->assertEquals($pdoAnimal->getAnimalBreed(), $this->VALID_ANIMAL_BREED);
-			$this->assertEquals($pdoAnimal->getAnimalGender(), $this->VALID_ANIMAL_GENDER);
-			$this->assertEquals($pdoAnimal->getAnimalName(), $this->VALID_ANIMAL_NAME);
-			$this->assertEquals($pdoAnimal->getAnimalPhotoUrl(), $this->VALID_ANIMAL_PHOTO_URL);
-			$this->assertEquals($pdoAnimal->getAnimalSpecies(), $this->VALID_ANIMAL_SPECIES);
-		}
+		$this->assertEquals($pdoAnimal->getAnimalId(), $animalId);
+		$this->assertEquals($pdoAnimal->getAnimalId(), $this->shelter->getAnimalShelterId());
+		$this->assertEquals($pdoAnimal->getAnimalAdoptionStatus(), $this->VALID_ANIMAL_ADOPTION_STATUS);
+		$this->assertEquals($pdoAnimal->getAnimalBreed(), $this->VALID_ANIMAL_BREED);
+		$this->assertEquals($pdoAnimal->getAnimalGender(), $this->VALID_ANIMAL_GENDER);
+		$this->assertEquals($pdoAnimal->getAnimalName(), $this->VALID_ANIMAL_NAME);
+		$this->assertEquals($pdoAnimal->getAnimalPhotoUrl(), $this->VALID_ANIMAL_PHOTO_URL);
+		$this->assertEquals($pdoAnimal->getAnimalSpecies(), $this->VALID_ANIMAL_SPECIES);
+	}
 
+	/**
+	 * Test grabbing animal by Shelter Id
+	 **/
+	public function testGetValidAnimalByLikeUserId(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("animal");
+
+		// create a new animal and insert to mySQL
+		$animalId = generateUuidV4();
+		$animal = new Animal($animalId, $this->like->likeUserId(), $this->animal->getAnimalId(), $this->VALID_ANIMAL_SHELTER_ID, $this->VALID_ANIMAL_ADOPTION_STATUS, $this->VALID_ANIMAL_BREED, $this->VALID_ANIMAL_GENDER, $this->VALID_ANIMAL_NAME, $this->VALID_ANIMAL_PHOTO_URL, $this->VALID_ANIMAL_SPECIES);
+		$animal->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Animal::getAnimalBylikeUserId($this->getPDO(), $animal->getAnimalShelterId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("animal"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("PawsForCause\Paws\Animal", $results);
+
+		// grab the result from the array and validate it
+		$pdoAnimal = $results[0];
+
+		$this->assertEquals($pdoAnimal->getAnimalId(), $animalId);
+		$this->assertEquals($pdoAnimal->getAnimalId(), $this->shelter->getAnimalShelterId());
+		$this->assertEquals($pdoAnimal->getAnimalAdoptionStatus(), $this->VALID_ANIMAL_ADOPTION_STATUS);
+		$this->assertEquals($pdoAnimal->getAnimalBreed(), $this->VALID_ANIMAL_BREED);
+		$this->assertEquals($pdoAnimal->getAnimalGender(), $this->VALID_ANIMAL_GENDER);
+		$this->assertEquals($pdoAnimal->getAnimalName(), $this->VALID_ANIMAL_NAME);
+		$this->assertEquals($pdoAnimal->getAnimalPhotoUrl(), $this->VALID_ANIMAL_PHOTO_URL);
+		$this->assertEquals($pdoAnimal->getAnimalSpecies(), $this->VALID_ANIMAL_SPECIES);
+	}
 }
