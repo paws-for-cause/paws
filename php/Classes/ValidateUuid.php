852f5abd75a54d0693e8ea26136338bea7c61064
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-13 14:48:03 -0700

    phpunit.xml now runs

--- php/Classes/ValidateUuid.php
+++ php/Classes/ValidateUuid.php
@@ -1,6 +1,7 @@
 <?php
    namespace PawsForCause\Paws;
-   require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+   require_once (dirname(__DIR__) . "/Classes/autoload.php");
+   require_once(dirname(__DIR__, 2) . "/php/vendor/autoload.php");
    use Ramsey\Uuid\Uuid;
    /**
     * Trait to validate a uuid
