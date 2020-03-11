533d547a8d2a8478a8fe262aa7d3fa7e1b5f0d84
ualnaji@cnm.edu <ualnaji@cnm.edu>
2020-03-11 13:15:36 -0600

    Added React Setup 2

--- app/src/index.js
+++ app/src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './App.css'
+import {StyleHTMLAttributes, ReactText, WebViewHTMLAttributes, ImgHTMLAttributes, AnimationEvent} from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
@@ -6,14 +8,45 @@ import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 
-const Routing = () => (
-    <>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route component={FourOhFour}/>
-            </Switch>
-        </BrowserRouter>
-    </>
-);
-ReactDOM.render(<Routing/>, document.querySelector('#root'));
+const App = () => {
+    const [animals, setAnimals] = useState(data);
+    const [likedAnimals, setLikedAnimals] = useState([]);
+    const [dislikeUsers, setDislikedUsers] = useState([]);
+    const activeUser = 0;
+
+    switch (action) {
+        case 'Add_To_Liked_Animal':
+            break;
+        case 'Add_To_Disliked_Animal':
+            break;
+        default:
+            return animals;
+    }
+
+    return (
+        <div className = "app">
+            <Header>
+
+                {animals[1] ? (
+                    <Animal
+                        key = {animal[1].id}
+                        aniaml = {animals[1]}
+                        modifyAnimalChoices = {modifyAnimalChoices}
+                        likedAnimals = {likedAnimals}
+                        />
+                ): (
+                    <nextAnimal
+                        activeAnimalImage = {animals[activeAnimal.image]}
+                    likedAnimals = {likedAnimals}
+                    />
+                )}
+            </Header>
+
+
+        </div>
+    )
+
+
+
+
+ReactDOM.render(<Routing/>, document.querySelector('#root'))};
