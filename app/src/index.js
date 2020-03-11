cc275908795fe200405eb310af818a51793fcce5
ualnaji@cnm.edu <ualnaji@cnm.edu>
2020-03-11 15:37:00 -0600

    Added React frontend boilerplate.

--- app/src/index.js
+++ app/src/index.js
@@ -1,52 +1,5 @@
-import React, {useState} from 'react';
-import './App.css'
-import {StyleHTMLAttributes, ReactText, WebViewHTMLAttributes, ImgHTMLAttributes, AnimationEvent} from 'react';
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from "react-router-dom";
-import {Route, Switch} from "react-router";
-import {FourOhFour} from "./pages/FourOhFour";
-import {Home} from "./pages/Home";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-const App = () => {
-    const [animals, setAnimals] = useState(data);
-    const [likedAnimals, setLikedAnimals] = useState([]);
-    const [dislikeUsers, setDislikedUsers] = useState([]);
-    const activeUser = 0;
-
-    switch (action) {
-        case 'Add_To_Liked_Animal':
-            break;
-        case 'Add_To_Disliked_Animal':
-            break;
-        default:
-            return animals;
-    }
-
-    return (
-        <div className = "app">
-            <Header>
-
-                {animals[1] ? (
-                    <Animal
-                        key = {animal[1].id}
-                        aniaml = {animals[1]}
-                        modifyAnimalChoices = {modifyAnimalChoices}
-                        likedAnimals = {likedAnimals}
-                        />
-                ): (
-                    <nextAnimal
-                        activeAnimalImage = {animals[activeAnimal.image]}
-                    likedAnimals = {likedAnimals}
-                    />
-                )}
-            </Header>
-
-
-        </div>
-    )
-
-
-
-
-ReactDOM.render(<Routing/>, document.querySelector('#root'))};
+render(<App />, document.getElementById('root'));
