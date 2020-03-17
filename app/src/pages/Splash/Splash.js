e19ebc9094580f756636578340a04346eea9b38e
Matthew Urrea <matt.urrea.code@gmail.com>
2020-03-17 10:44:07 -0600

    added styling

--- app/src/pages/Splash/Splash.js
+++ app/src/pages/Splash/Splash.js
@@ -2,19 +2,23 @@ import React from "react"
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
+import "./Splash.css"
 
 const Splash = () => {
 	return (
 		<>
-			<Container>
-				<main className="text-center mt-10">
-				<h1>P.A.W.S.</h1>
-				<h3>Pet Adoption With Software</h3>
-					<Button variant="primary" type="link"><a href="sign-in">Enter</a></Button>
+
+			<Container className="splash-bg">
+				<main className="splash-main text-center mt-10">
+					<h1>P.A.W.S.</h1>
+					<h3>Pet Adoption and Welfare Solutions</h3>
+					<Button variant="primary" type="link">
+						<a className="splash-link" href="sign-in">Enter</a>
+					</Button>
 				</main>
 			</Container>
 
-			</>
+		</>
 	)
 }
 
