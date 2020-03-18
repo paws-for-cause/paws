93d8d14fd328b45c42dcf6ba1d918a71eb537558
Thomas Dameron <tdameron1@cnm.edu>
2020-03-18 13:07:03 -0600

    Changed logo to a link, changed size of logo and some text formatting, changed logo to a .png

--- app/src/pages/Splash/Splash.js
+++ app/src/pages/Splash/Splash.js
@@ -7,7 +7,9 @@ import "./Splash.css"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import SplashLogo from '../page-images/Paws Image 1.jpg'
+import SplashLogo from '../page-images/Paws Image 1.png'
+
+import PawsLogo from '../page-images/logo.png'
 
 const Splash = () => {
 	return (
@@ -17,12 +19,14 @@ const Splash = () => {
 				<main>
 					<Row>
 						<Col sm={4} md={4} lg={12}>
+							<a href="sign-in">
 								<img src={SplashLogo} className="splash-pets" alt ='Test Page image'/>
+							</a>
 								<h1 className="splash-text">P.A.W.S.</h1>
 								<p>Pet Adoption and Welfare Solutions</p>
 						</Col>
 					</Row>
-					<Button variant="primary" type="link">
+					<Button>
 						<a className="splash-link" href="sign-in">Enter</a>
 					</Button>
 				</main>
