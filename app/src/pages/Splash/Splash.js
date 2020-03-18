d5b72ea99e444ca4c350663f2d322dcfba2a0a2f
Thomas Dameron <tdameron1@cnm.edu>
2020-03-18 08:33:42 -0600

    Latest commit out of fear that PHP Storm might crash.

--- app/src/pages/Splash/Splash.js
+++ app/src/pages/Splash/Splash.js
@@ -14,7 +14,7 @@ const Splash = () => {
 				<main>
 					<h1>P.A.W.S.</h1>
 					<h3>Pet Adoption and Welfare Solutions</h3>
-					<img src={SplashLogo}  alt ='Test Page image'/>
+						<img src={SplashLogo} className="splash-pets" alt ='Test Page image'/>
 					<Button variant="primary" type="link">
 						<a className="splash-link" href="sign-in">Enter</a>
 					</Button>
