8187216d20b1459beee00379a4e5410d22fb5ef9
ualnaji@cnm.edu <ualnaji@cnm.edu>
2020-03-18 12:46:30 -0600

    Started Bookmark Redux process.

---
+++ app/src/pages/Bookmarks/UserList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+const UserListComponent = ({users}) => {
+
+    return (
+        <Route render={ ({history}) => (
+            <tbody>
+            {users.map(user => (
+                <tr key={user.userId} onClick={() => {history.push(`user/${user.userId}`)}}>
+                    <td>{user.userId}</td>
+                    <td>{user.age}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                    <td>{user.username}</td>
+                    <td>{user.hash}</td>
+                </tr>
+            ))}
+            </tbody>
+        )}/>
+    )
+};
+
+export const UserList = (UserListComponent);
