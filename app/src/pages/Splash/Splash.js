deb99e15703108c05257f1f340ce3b165119ec2c
Thomas Dameron <tdameron1@cnm.edu>
2020-03-17 21:12:04 -0600

    Added Images to Use for Web Pages

--- app/src/pages/Splash/Splash.js
+++ app/src/pages/Splash/Splash.js
@@ -7,17 +7,18 @@ import "./Splash.css"
 const Splash = () => {
 	return (
 		<>
-
+<body>
 			<Container className="splash-bg">
 				<main className="splash-main text-center mt-10">
 					<h1>P.A.W.S.</h1>
 					<h3>Pet Adoption and Welfare Solutions</h3>
+					<img src={require("images/page-images/Paws Image 1")} alt ='Test Page image'/>
 					<Button variant="primary" type="link">
 						<a className="splash-link" href="sign-in">Enter</a>
 					</Button>
 				</main>
 			</Container>
-
+</body>
 		</>
 	)
 }
