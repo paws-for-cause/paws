be011eabf0d64622b29ade24a9444f4353dbbf9e
Thomas Dameron <tdameron1@cnm.edu>
2020-03-17 21:50:43 -0600

    Moved Page Images to src so that they will load

--- app/src/pages/Splash/Splash.js
+++ app/src/pages/Splash/Splash.js
@@ -4,23 +4,26 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import "./Splash.css"
 
+import SplashLogo from '../page-images/Paws Image 1.jpg'
+
 const Splash = () => {
 	return (
 		<>
-<body>
+
 			<Container className="splash-bg">
 				<main className="splash-main text-center mt-10">
 					<h1>P.A.W.S.</h1>
 					<h3>Pet Adoption and Welfare Solutions</h3>
-					<img src={require("images/page-images/Paws Image 1")} alt ='Test Page image'/>
+					<img src={SplashLogo} alt ='Test Page image'/>
 					<Button variant="primary" type="link">
 						<a className="splash-link" href="sign-in">Enter</a>
 					</Button>
 				</main>
 			</Container>
-</body>
 		</>
 	)
-}
+};
+
+
 
 export default Splash;
